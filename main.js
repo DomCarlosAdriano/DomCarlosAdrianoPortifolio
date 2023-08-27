311b8fa7336d111804c05724abf793e847a23227
Dom Carlos Adriano <dogmalcarlos@gmail.com>
2023-08-26 23:39:02 -0300

    first test responsive

---
+++ main.js
@@ -0,0 +1,5 @@
+const btnMenuMobile = document.querySelector(".butao-menu-mobile")
+const navMenuMobile = document.querySelector(".nav-menu-mobile")
+const navMenuDesktop = document.querySelector(".nav-menu-desktop")
+const containerApresentacao = document.querySelector(".container-apresentacao")
+const setaParaBaixo = document.querySelector(".seta-para-baixo")
