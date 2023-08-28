80f75d6f39e9cdc776c34e6cd8d1fd93aae4bb08
Dom Carlos Adriano <dogmalcarlos@gmail.com>
2023-08-28 01:17:51 -0300

    section projetos

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ const navMenuMobile = document.querySelector(".nav-menu-mobile")
 const navMenuDesktop = document.querySelector(".nav-menu-desktop")
 const containerApresentacao = document.querySelector(".container-apresentacao")
 const setaParaBaixo = document.querySelector(".seta-para-baixo")
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//    const scrollbar1 = document.querySelector(".swiper-horizontal>.swiper-scrollbar, .swiper-scrollbar.swiper-scrollbar-horizontal")
+//    scrollbar1.style.background = "#6699ff"
+   
+//    const scrollbar2 = document.querySelector(".swiper-scrollbar swiper-scrollbar-horizontal")
+//    scrollbar2.style.background = "#6699ff"
+   
+// });
